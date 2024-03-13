279fc5ec978c039651e71aa234e0edca4c96e693
hsnpuply <hsnpuply@gmail.com>
2024-03-13 14:09:59 +0330

    new products section

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,14 +15,24 @@ module.exports = {
       backgroundImage:{
         'iPhone15_Pro' :"url('images/iPhone15_Pro.jpg')",
         'iPhone15' :"url('images/iPhone15.jpg')",
+        'watch_bg':"url('images/appleWatchHero.jpg')",
+        'produc_item-1':"url('images/visionPro.jpg')",
+        'produc_item-2':"url('images/macbook.jpg')",
+        'produc_item-3':"url('images/iPad.jpg')",
+        'produc_item-4':"url('images/iPadPro.jpg')",
+        'produc_item-5':"url('images/card.jpg')",
+        'produc_item-6':"url('images/tradein.jpg')",
 
       },
-      width:{
-        'FullPage':'100vw'
+      minWidth:{
+        'new_product-w':'702px'
+      },
+      minHeight:{
+        'new_product-h':'580px'
       },
       height:{
         'i15Pro':'77vh',
-        'i15':'74vh'
+        'i15':'74vh',
       }
 
     },
