b75d376cbabc40f485052b60e7332a440ff97e40
hsnpuply <hsnpuply@gmail.com>
2024-03-17 02:17:29 +0330

    fix links
    fix links hover

---
+++ dist/script.js
@@ -0,0 +1,11 @@
+// const logo_link = document.querySelector('#logo-link');
+// const logo_svg = document.querySelector('#logo-svg');
+
+
+
+
+
+
+// logo_link.addEventListener('mouseover', () => {
+//     logo_svg.style.fill = '#000000';
+// })
